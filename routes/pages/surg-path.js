f02b0d6b60ce1c393a8203781a9a95cb66bd6e90
Sarah Riehl <sls.riehl@gmail.com>
2017-08-16 15:25:13 -0500

    routes reorg, surg path routes

---
+++ routes/pages/surg-path.js
@@ -0,0 +1,57 @@
+
+const express = require('express');
+
+const router = new express.Router;
+
+//needs database connection
+router.get('/surg-path/ihc-table', (req, res) => {
+	//initial page load with data: controller: pull data from mysql, template results
+	//Report saving & PDFing: AJAX & controller: front end script to generate report, AJAX POST to controller to store in DB per person, generate PDF report & send to file system, and template success/result for download
+	//adding data to DB: AJAX & controller: front end script to receive new data, AJAX POST to controller to store in DB per person, template success
+	//initial page load with data: controller: if the user has previous reports, pull data & template results including download button
+	//previous report handling: AJAX & controller: if the user selects a previous report, template text from DB & populate generate report button autodownload
+	//placeholder data
+	const data = [{
+			'opt': 'foo',
+			'name': 'bar',
+			'interp':'baz'
+		}];
+	res.render('./page-views/surg-path/ihc-table.hbs',
+	{"specificScripts":
+		[
+			"../js/surg-path/ihc-scripts.js"
+		],
+		'data': data
+	});
+});
+
+router.get('/surg-path/fixation', (req, res) => {
+	//Report saving & PDFing: AJAX & controller: front end script to generate report, AJAX POST to controller to store in DB per person, generate PDF report & send to file system, and template success/result for download
+	//initial page load with data: controller: if the user has previous reports, pull data & template results including download button
+	//previous report handling: AJAX & controller: if the user selects a previous report, template text from DB & populate generate report button auto download
+	//placeholder data
+	res.render('./page-views/surg-path/fixation.hbs',
+	{"specificScripts":
+		[
+			"../vendor/jquery/js/jquery-ui-timepicker-addon.min.js",
+			"../js/surg-path/fixation-scripts.js"
+		]
+	});
+});
+
+
+router.get('/surg-path/snippets', (req, res) => {
+	//search: AJAX & controller: POST search to controller, search DB, template results with delete buttons if same users & not if Chandra's, & view/edit buttons
+	//custom helper:  if view/edit button is pushed, template fields into text areas, add generate PDF button
+	//AJAX & controller: if snippet is saved save/overwrite in controller/DB (modal for save, save as, & cancel), generate PDF button, template success & download button
+	//download controller/route path
+	res.render('./page-views/surg-path/snippets.hbs', {"specificScripts":
+		[
+			"../vendor/tag-it/tag-it.min.js",
+			"../js/surg-path/snippets-scripts.js"
+		]
+	});
+});
+
+
+module.exports = router;
