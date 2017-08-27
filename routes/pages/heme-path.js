4928487314bbbdd45637a5942cd1aba2bbf9d56f
Sarah Riehl <sls.riehl@gmail.com>
2017-08-26 22:08:17 -0500

    add heme-diff routes for copying text from previous reports

--- routes/pages/heme-path.js
+++ routes/pages/heme-path.js
@@ -4,10 +4,9 @@ const router = new express.Router;
 
 const controller = require('../../controllers/pages');
 
+
+//routes to get the pages to create a fresh report
 router.get('/heme-path/pb-smears', (req, res) => {
-	//Report saving & PDFing: AJAX & controller: front end script to generate report, AJAX POST to controller to store in DB per person, generate PDF report & send to file system, and template success/result for download
-	//initial page load with data: controller: if the user has previous reports, pull data & template results including download button
-	//previous report handling: AJAX & controller: if the user selects a previous report, template text from DB & populate generate report button auto download
 	controller.openAccess(req, res, './page-views/heme-path/pb-smears.hbs', [
 		"../json/json-pbsmears.js",
 		"../js/heme-path/pb-rules.js",
@@ -16,9 +15,6 @@ router.get('/heme-path/pb-smears', (req, res) => {
 });
 
 router.get('/heme-path/bone-marrow', (req, res) => {
-	//Report saving & PDFing: AJAX & controller: front end script to generate report, AJAX POST to controller to store in DB per person, generate PDF report & send to file system, and template success/result for download
-	//initial page load with data: controller: if the user has previous reports, pull data & template results including download button
-	//previous report handling: AJAX & controller: if the user selects a previous report, template text from DB & populate generate report button auto download
 	controller.openAccess(req, res, './page-views/heme-path/bone-marrow.hbs', [
 		"../json/json-bonemarrow.js",
 		"../js/heme-path/bm-diff-scripts.js",
@@ -28,27 +24,54 @@ router.get('/heme-path/bone-marrow', (req, res) => {
 });
 
 router.get('/heme-path/heme-diff', (req, res) => {
-	//Report saving & PDFing: AJAX & controller: front end script to generate report, AJAX POST to controller to store in DB per person, generate PDF report & send to file system, and template success/result for download
-	//initial page load with data: controller: if the user has previous reports, pull data & template results including download button
-	//previous report handling: AJAX & controller: if the user selects a previous report, template text from DB & populate generate report button auto download
 	controller.openAccess(req, res, './page-views/heme-path/heme-diff.hbs', [
 		"../js/heme-path/hemediff-scripts.js"
 	]);
 });
 
 router.get('/heme-path/counter', (req, res) => {
-	//Report saving & PDFing: AJAX & controller: front end script to generate report, AJAX POST to controller to store in DB per person, generate PDF report & send to file system, and template success/result for download
-	//initial page load with data: controller: if the user has previous reports, pull data & template results including download button
-	//previous report handling: AJAX & controller: if the user selects a previous report, template text from DB & populate generate report button auto download
 	controller.openAccess(req, res, './page-views/heme-path/counter.hbs', [
 		"../js/heme-path/counter-scripts.js"
 	]);
 });
 
 router.get('/heme-path/dlbcl', (req, res) => {
-	//Report saving & PDFing: AJAX & controller: front end script to generate report, AJAX POST to controller to store in DB per person, generate PDF report & send to file system, and template success/result for download
-	//initial page load with data: controller: if the user has previous reports, pull data & template results including download button
-	//previous report handling: AJAX & controller: if the user selects a previous report, template text from DB & populate generate report button auto download
+	controller.userWall(req, res, './page-views/heme-path/dlbcl.hbs', [
+		"../js/heme-path/dlbcl-scripts.js"
+	]);
+});
+
+//routes to get the pages to copy plain text from a specific previous report
+router.get('/heme-path/pb-smears/:id', (req, res) => {
+	controller.openAccess(req, res, './page-views/heme-path/pb-smears.hbs', [
+		"../json/json-pbsmears.js",
+		"../js/heme-path/pb-rules.js",
+		"../js/heme-path/pb-scripts.js"
+	]);
+});
+
+router.get('/heme-path/bone-marrow/:id', (req, res) => {
+	controller.openAccess(req, res, './page-views/heme-path/bone-marrow.hbs', [
+		"../json/json-bonemarrow.js",
+		"../js/heme-path/bm-diff-scripts.js",
+		"../js/heme-path/bm-scripts.js",
+		"../js/heme-path/pb-rules.js"
+	]);
+});
+
+router.get('/heme-path/heme-diff/:id', (req, res) => {
+	controller.openAccess(req, res, './page-views/heme-path/heme-diff.hbs', [
+		"../js/heme-path/hemediff-scripts.js"
+	]);
+});
+
+router.get('/heme-path/counter/:id', (req, res) => {
+	controller.openAccess(req, res, './page-views/heme-path/counter.hbs', [
+		"../js/heme-path/counter-scripts.js"
+	]);
+});
+
+router.get('/heme-path/dlbcl/:id', (req, res) => {
 	controller.userWall(req, res, './page-views/heme-path/dlbcl.hbs', [
 		"../js/heme-path/dlbcl-scripts.js"
 	]);
