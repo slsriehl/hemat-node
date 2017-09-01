85802a113e9bd284115155cfa00c3ae452d641a6
Sarah Riehl <sls.riehl@gmail.com>
2017-08-31 22:16:18 -0500

    antibody id front/back integration

--- public/js/clin-path/antibody-scripts.js
+++ public/js/clin-path/antibody-scripts.js
@@ -1,6 +1,9 @@
-$('#antibody').on('change', function(){
-		var sel = $(this).val();
+$('#writeReport').on('click', function(){
+		var sel = $('#antibody').val();
 		var diag = dxLines["dxLine"+sel];
 		var comment = comLines["comLine"+sel];
 		$('#outPut-1').val(diag+'\n\n'+comment);
+
+		dataObj.singleSection = $('#outPut-1').val();
+		makeCreatePdfBtn();
 });
