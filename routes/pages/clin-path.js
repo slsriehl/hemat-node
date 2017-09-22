b8fd407ea99affb193a48a4ca41fe89a400e25eb
Sarah Riehl <sls.riehl@gmail.com>
2017-09-21 20:21:21 -0500

    remove todo notes from some routes

--- routes/pages/clin-path.js
+++ routes/pages/clin-path.js
@@ -5,9 +5,6 @@ const router = new express.Router;
 const controller = require('../../controllers/pages');
 
 router.get('/clin-path/csf', (req, res) => {
-	//Report saving & PDFing: AJAX & controller: front end script to generate report, AJAX POST to controller to store in DB per person, generate PDF report & send to file system, and template success/result for download
-	//initial page load with data: controller: if the user has previous reports, pull data & template results including download button
-	//previous report handling: AJAX & controller: if the user selects a previous report, template text from DB & populate generate report button auto download
 	controller.openAccess(req, res, './page-views/clin-path/csf.hbs', [
 		"/json/json-csf.js",
 		"/js/clin-path/csf-scripts.js"
@@ -15,9 +12,6 @@ router.get('/clin-path/csf', (req, res) => {
 });
 
 router.get('/clin-path/bal', (req, res) => {
-	//Report saving & PDFing: AJAX & controller: front end script to generate report, AJAX POST to controller to store in DB per person, generate PDF report & send to file system, and template success/result for download
-	//initial page load with data: controller: if the user has previous reports, pull data & template results including download button
-	//previous report handling: AJAX & controller: if the user selects a previous report, template text from DB & populate generate report button auto download
 	controller.openAccess(req, res, './page-views/clin-path/bal.hbs', [
 		"/json/json-bal.js",
 		"/js/clin-path/bal-scripts.js"
@@ -25,9 +19,6 @@ router.get('/clin-path/bal', (req, res) => {
 });
 
 router.get('/clin-path/antibody-id', (req, res) => {
-	//Report saving & PDFing: AJAX & controller: front end script to generate report, AJAX POST to controller to store in DB per person, generate PDF report & send to file system, and template success/result for download
-	//initial page load with data: controller: if the user has previous reports, pull data & template results including download button
-	//previous report handling: AJAX & controller: if the user selects a previous report, template text from DB & populate generate report button auto download
 	controller.openAccess(req, res, './page-views/clin-path/antibody-id.hbs', [
 		"/json/json-abid.js",
 		"/js/clin-path/antibody-scripts.js"
@@ -35,9 +26,6 @@ router.get('/clin-path/antibody-id', (req, res) => {
 });
 
 router.get('/clin-path/tx-rxn', (req, res) => {
-	//Report saving & PDFing: AJAX & controller: front end script to generate report, AJAX POST to controller to store in DB per person, generate PDF report & send to file system, and template success/result for download
-	//initial page load with data: controller: if the user has previous reports, pull data & template results including download button
-	//previous report handling: AJAX & controller: if the user selects a previous report, template text from DB & populate generate report button auto download
 	controller.openAccess(req, res, './page-views/clin-path/tx-rxn.hbs', [
 		"/json/json-txrxn.js",
 		"/js/clin-path/txrxn-scripts.js"
--- routes/pages/gi-path.js
+++ routes/pages/gi-path.js
@@ -5,10 +5,6 @@ const router = new express.Router;
 const controller = require('../../controllers/pages');
 
 router.get('/gi-path/gi-bx', (req, res) => {
-	//IHC modal routes
-	//Report saving & PDFing: AJAX & controller: front end script to generate report, AJAX POST to controller to store in DB per person, generate PDF report & send to file system, and template success/result for download
-	//initial page load with data: controller: if the user has previous reports, pull data & template results including download button
-	//previous report handling: AJAX & controller: if the user selects a previous report, template text from DB & populate generate report button auto download
 	controller.openAccess(req, res, './page-views/gi-path/gi-bx.hbs', [
 		"/json/json-gi.js",
 		"/js/gi-path/gi-scripts.js",
@@ -17,9 +13,6 @@ router.get('/gi-path/gi-bx', (req, res) => {
 });
 
 router.get('/gi-path/liver-bx', (req, res) => {
-	//Report saving & PDFing: AJAX & controller: front end script to generate report, AJAX POST to controller to store in DB per person, generate PDF report & send to file system, and template success/result for download
-	//initial page load with data: controller: if the user has previous reports, pull data & template results including download button
-	//previous report handling: AJAX & controller: if the user selects a previous report, template text from DB & populate generate report button auto download
 	controller.userWall(req, res, './page-views/gi-path/liver-bx.hbs', [
 		"/json/json-liver.js",
 		"/js/gi-path/liver-scripts.js",
