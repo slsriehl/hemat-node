c9edbbbba7e7e76661ef524b9dca51983eff0483
Sarah Riehl <sls.riehl@gmail.com>
2017-09-25 21:16:57 -0500

    pages-get test running omnibus

--- test/pages-get.js
+++ test/pages-get.js
@@ -12,10 +12,17 @@ const giPathRightCookie = require('./pages-get/right-cookie/gi-path');
 const clinPathNoCookie = require('./pages-get/no-cookie/clin-path');
 const clinPathWrongCookie = require('./pages-get/wrong-cookie/clin-path');
 const clinPathRightCookie = require('./pages-get/right-cookie/clin-path');
-//surg-path pages
 //cap pages
-
-
+const capOneNoCookie = require('./pages-get/no-cookie/cap-one');
+const capTwoNoCookie = require('./pages-get/no-cookie/cap-two');
+const capOneWrongCookie = require('./pages-get/wrong-cookie/cap-one');
+const capTwoWrongCookie = require('./pages-get/wrong-cookie/cap-two');
+const capOneRightCookie = require('./pages-get/right-cookie/cap-one');
+const capTwoRightCookie = require('./pages-get/right-cookie/cap-two');
+//surg-path pages
+const surgPathNoCookie = require('./pages-get/no-cookie/surg-path');
+const surgPathWrongCookie = require('./pages-get/wrong-cookie/surg-path');
+const surgPathRightCookie = require('./pages-get/right-cookie/surg-path');
 
 const beforeEachHooks = require('./helpers/before-each');
 
@@ -59,6 +66,110 @@ describe('logged in get requests', function () {
 	it('should render the tx-rxn page', function(done) {
 		clinPathRightCookie.txRxn(done);
 	});
+	it('should show a user wall on the bone-t-bx page and render the main page', function(done) {
+		capOneRightCookie.boneTBx(done);
+	});
+	it('should show a user wall on the bone-t-rx page and render the main page', function(done) {
+		capOneRightCookie.boneTRx(done);
+	});
+	it('should show a user wall on the neuro-t page and render the main page', function(done) {
+		capOneRightCookie.neuroT(done);
+	});
+	it('should show a user wall on the neuro-t-biomarker page and render the main page', function(done) {
+		capOneRightCookie.neuroTBio(done);
+	});
+	it('should show a user wall on the pnet-bx page and render the main page', function(done) {
+		capOneRightCookie.pnetBx(done);
+	});
+	it('should show a user wall on the pnet-rx page and render the main page', function(done) {
+		capOneRightCookie.pnetRx(done);
+	});
+	it('should show a user wall on the ex-gon-gct page and render the main page', function(done) {
+		capOneRightCookie.exGonGct(done);
+	});
+	it('should show a user wall on the hepatoblast page and render the main page', function(done) {
+		capOneRightCookie.hepatoBlast(done);
+	});
+	it('should show a user wall on the neuroblast page and render the main page', function(done) {
+		capOneRightCookie.neuroBlast(done);
+	});
+	it('should show a user wall on the neuroend-t page and render the main page', function(done) {
+		capOneRightCookie.neuroEndT(done);
+	});
+	it('should show a user wall on the ovary-t page and render the main page', function(done) {
+		capOneRightCookie.ovaryT(done);
+	});
+	it('should show a user wall on the wilms-t page and render the main page', function(done) {
+		capOneRightCookie.wilmsT(done);
+	});
+	it('should show a user wall on the retinoblast page and render the main page', function(done) {
+		capOneRightCookie.retinoBlast(done);
+	});
+	it('should show a user wall on the rhabdo page and render the main page', function(done) {
+		capOneRightCookie.rhabdo(done);
+	});
+	it('should show a user wall on the testis-t page and render the main page', function(done) {
+		capOneRightCookie.testisT(done);
+	});
+	it('should show a user wall on the soft-t-rx page and render the main page', function(done) {
+		capOneRightCookie.softTRx(done);
+	});
+	//cap two adult only
+	it('should show a user wall on the breast page and render the main page', function(done) {
+		capTwoRightCookie.breast(done);
+	});
+	it('should show a user wall on the cervix-leep page and render the main page', function(done) {
+		capTwoRightCookie.cervixLeep(done);
+	});
+	it('should show a user wall on the cervix-rx page and render the main page', function(done) {
+		capTwoRightCookie.cervixRx(done);
+	});
+	it('should show a user wall on the colon-polyp page and render the main page', function(done) {
+		capTwoRightCookie.colonPolyp(done);
+	});
+	it('should show a user wall on the colon-rx page and render the main page', function(done) {
+		capTwoRightCookie.colonRx(done);
+	});
+	it('should show a user wall on the endometrium-t page and render the main page', function(done) {
+		capTwoRightCookie.endometriumT(done);
+	});
+	it('should show a user wall on the esophagus page and render the main page', function(done) {
+		capTwoRightCookie.esophagus(done);
+	});
+	it('should show a user wall on the kidney-rx page and render the main page', function(done) {
+		capTwoRightCookie.kidneyRx(done);
+	});
+	it('should show a user wall on the lung-rx page and render the main page', function(done) {
+		capTwoRightCookie.lungRx(done);
+	});
+	it('should show a user wall on the melanoma page and render the main page', function(done) {
+		capTwoRightCookie.melanoma(done);
+	});
+	it('should show a user wall on the plasmacytoma page and render the main page', function(done) {
+		capTwoRightCookie.plasmacytoma(done);
+	});
+	it('should show a user wall on the pancexo-t page and render the main page', function(done) {
+		capTwoRightCookie.pancExoT(done);
+	});
+	it('should show a user wall on the thyroid-t page and render the main page', function(done) {
+		capTwoRightCookie.thyroidT(done);
+	});
+	it('should show a user wall on the salivary-t page and render the main page', function(done) {
+		capTwoRightCookie.salivaryT(done);
+	});
+	it('should show a user wall on the stomach page and render the main page', function(done) {
+		capTwoRightCookie.stomach(done);
+	});
+	//surg path
+	it('should show a user wall on the ihc-tables page and render the main page', function(done) {
+		surgPathRightCookie.ihc(done);
+	});
+	it('should render the breast fixation page without login features', function(done) {
+		surgPathRightCookie.breastFixation(done);
+	});
+	it('should show a user wall on the snippets page and render the main page', function(done) {
+		surgPathRightCookie.snippets(done);
+	});
 });
 
 describe('expired cookie get requests', function() {
@@ -101,8 +212,112 @@ describe('expired cookie get requests', function() {
 	it('should render the tx-rxn page', function(done) {
 		clinPathWrongCookie.txRxn(done);
 	});
+	it('should show a user wall on the bone-t-bx page and render the main page', function(done) {
+		capOneWrongCookie.boneTBx(done);
+	});
+	it('should show a user wall on the bone-t-rx page and render the main page', function(done) {
+		capOneWrongCookie.boneTRx(done);
+	});
+	it('should show a user wall on the neuro-t page and render the main page', function(done) {
+		capOneWrongCookie.neuroT(done);
+	});
+	it('should show a user wall on the neuro-t-biomarker page and render the main page', function(done) {
+		capOneWrongCookie.neuroTBio(done);
+	});
+	it('should show a user wall on the pnet-bx page and render the main page', function(done) {
+		capOneWrongCookie.pnetBx(done);
+	});
+	it('should show a user wall on the pnet-rx page and render the main page', function(done) {
+		capOneWrongCookie.pnetRx(done);
+	});
+	it('should show a user wall on the ex-gon-gct page and render the main page', function(done) {
+		capOneWrongCookie.exGonGct(done);
+	});
+	it('should show a user wall on the hepatoblast page and render the main page', function(done) {
+		capOneWrongCookie.hepatoBlast(done);
+	});
+	it('should show a user wall on the neuroblast page and render the main page', function(done) {
+		capOneWrongCookie.neuroBlast(done);
+	});
+	it('should show a user wall on the neuroend-t page and render the main page', function(done) {
+		capOneWrongCookie.neuroEndT(done);
+	});
+	it('should show a user wall on the ovary-t page and render the main page', function(done) {
+		capOneWrongCookie.ovaryT(done);
+	});
+	it('should show a user wall on the wilms-t page and render the main page', function(done) {
+		capOneWrongCookie.wilmsT(done);
+	});
+	it('should show a user wall on the retinoblast page and render the main page', function(done) {
+		capOneWrongCookie.retinoBlast(done);
+	});
+	it('should show a user wall on the rhabdo page and render the main page', function(done) {
+		capOneWrongCookie.rhabdo(done);
+	});
+	it('should show a user wall on the testis-t page and render the main page', function(done) {
+		capOneWrongCookie.testisT(done);
+	});
+	it('should show a user wall on the soft-t-rx page and render the main page', function(done) {
+		capOneWrongCookie.softTRx(done);
+	});
+	//cap two adult only
+	it('should show a user wall on the breast page and render the main page', function(done) {
+		capTwoWrongCookie.breast(done);
+	});
+	it('should show a user wall on the cervix-leep page and render the main page', function(done) {
+		capTwoWrongCookie.cervixLeep(done);
+	});
+	it('should show a user wall on the cervix-rx page and render the main page', function(done) {
+		capTwoWrongCookie.cervixRx(done);
+	});
+	it('should show a user wall on the colon-polyp page and render the main page', function(done) {
+		capTwoWrongCookie.colonPolyp(done);
+	});
+	it('should show a user wall on the colon-rx page and render the main page', function(done) {
+		capTwoWrongCookie.colonRx(done);
+	});
+	it('should show a user wall on the endometrium-t page and render the main page', function(done) {
+		capTwoWrongCookie.endometriumT(done);
+	});
+	it('should show a user wall on the esophagus page and render the main page', function(done) {
+		capTwoWrongCookie.esophagus(done);
+	});
+	it('should show a user wall on the kidney-rx page and render the main page', function(done) {
+		capTwoWrongCookie.kidneyRx(done);
+	});
+	it('should show a user wall on the lung-rx page and render the main page', function(done) {
+		capTwoWrongCookie.lungRx(done);
+	});
+	it('should show a user wall on the melanoma page and render the main page', function(done) {
+		capTwoWrongCookie.melanoma(done);
+	});
+	it('should show a user wall on the plasmacytoma page and render the main page', function(done) {
+		capTwoWrongCookie.plasmacytoma(done);
+	});
+	it('should show a user wall on the pancexo-t page and render the main page', function(done) {
+		capTwoWrongCookie.pancExoT(done);
+	});
+	it('should show a user wall on the thyroid-t page and render the main page', function(done) {
+		capTwoWrongCookie.thyroidT(done);
+	});
+	it('should show a user wall on the salivary-t page and render the main page', function(done) {
+		capTwoWrongCookie.salivaryT(done);
+	});
+	it('should show a user wall on the stomach page and render the main page', function(done) {
+		capTwoWrongCookie.stomach(done);
+	});
+	//surg-path
+	it('should show a user wall on the ihc-tables page and render the main page', function(done) {
+		surgPathWrongCookie.ihc(done);
+	});
+	it('should render the breast fixation page without login features', function(done) {
+		surgPathWrongCookie.breastFixation(done);
+	});
+	it('should show a user wall on the snippets page and render the main page', function(done) {
+		surgPathWrongCookie.snippets(done);
+	});
 });
-//
+
 describe('no cookie get requests', function() {
 	//no before each because there's no db considerations
 	//heme-path pages
@@ -144,5 +359,110 @@ describe('no cookie get requests', function() {
 	});
 	it('should render the tx-rxn page', function(done) {
 		clinPathNoCookie.txRxn(done);
+	});
+	//cap one
+	it('should show a user wall on the bone-t-bx page and render the main page', function(done) {
+		capOneNoCookie.boneTBx(done);
+	});
+	it('should show a user wall on the bone-t-rx page and render the main page', function(done) {
+		capOneNoCookie.boneTRx(done);
+	});
+	it('should show a user wall on the neuro-t page and render the main page', function(done) {
+		capOneNoCookie.neuroT(done);
+	});
+	it('should show a user wall on the neuro-t-biomarker page and render the main page', function(done) {
+		capOneNoCookie.neuroTBio(done);
+	});
+	it('should show a user wall on the pnet-bx page and render the main page', function(done) {
+		capOneNoCookie.pnetBx(done);
+	});
+	it('should show a user wall on the pnet-rx page and render the main page', function(done) {
+		capOneNoCookie.pnetRx(done);
+	});
+	it('should show a user wall on the ex-gon-gct page and render the main page', function(done) {
+		capOneNoCookie.exGonGct(done);
+	});
+	it('should show a user wall on the hepatoblast page and render the main page', function(done) {
+		capOneNoCookie.hepatoBlast(done);
+	});
+	it('should show a user wall on the neuroblast page and render the main page', function(done) {
+		capOneNoCookie.neuroBlast(done);
+	});
+	it('should show a user wall on the neuroend-t page and render the main page', function(done) {
+		capOneNoCookie.neuroEndT(done);
+	});
+	it('should show a user wall on the ovary-t page and render the main page', function(done) {
+		capOneNoCookie.ovaryT(done);
+	});
+	it('should show a user wall on the wilms-t page and render the main page', function(done) {
+		capOneNoCookie.wilmsT(done);
+	});
+	it('should show a user wall on the retinoblast page and render the main page', function(done) {
+		capOneNoCookie.retinoBlast(done);
+	});
+	it('should show a user wall on the rhabdo page and render the main page', function(done) {
+		capOneNoCookie.rhabdo(done);
+	});
+	it('should show a user wall on the testis-t page and render the main page', function(done) {
+		capOneNoCookie.testisT(done);
+	});
+	it('should show a user wall on the soft-t-rx page and render the main page', function(done) {
+		capOneNoCookie.softTRx(done);
+	});
+	//cap two adult only
+	it('should show a user wall on the breast page and render the main page', function(done) {
+		capTwoNoCookie.breast(done);
+	});
+	it('should show a user wall on the cervix-leep page and render the main page', function(done) {
+		capTwoNoCookie.cervixLeep(done);
+	});
+	it('should show a user wall on the cervix-rx page and render the main page', function(done) {
+		capTwoNoCookie.cervixRx(done);
+	});
+	it('should show a user wall on the colon-polyp page and render the main page', function(done) {
+		capTwoNoCookie.colonPolyp(done);
+	});
+	it('should show a user wall on the colon-rx page and render the main page', function(done) {
+		capTwoNoCookie.colonRx(done);
+	});
+	it('should show a user wall on the endometrium-t page and render the main page', function(done) {
+		capTwoNoCookie.endometriumT(done);
+	});
+	it('should show a user wall on the esophagus page and render the main page', function(done) {
+		capTwoNoCookie.esophagus(done);
+	});
+	it('should show a user wall on the kidney-rx page and render the main page', function(done) {
+		capTwoNoCookie.kidneyRx(done);
+	});
+	it('should show a user wall on the lung-rx page and render the main page', function(done) {
+		capTwoNoCookie.lungRx(done);
+	});
+	it('should show a user wall on the melanoma page and render the main page', function(done) {
+		capTwoNoCookie.melanoma(done);
+	});
+	it('should show a user wall on the plasmacytoma page and render the main page', function(done) {
+		capTwoNoCookie.plasmacytoma(done);
+	});
+	it('should show a user wall on the pancexo-t page and render the main page', function(done) {
+		capTwoNoCookie.pancExoT(done);
+	});
+	it('should show a user wall on the thyroid-t page and render the main page', function(done) {
+		capTwoNoCookie.thyroidT(done);
+	});
+	it('should show a user wall on the salivary-t page and render the main page', function(done) {
+		capTwoNoCookie.salivaryT(done);
+	});
+	it('should show a user wall on the stomach page and render the main page', function(done) {
+		capTwoNoCookie.stomach(done);
+	});
+	//surg-path
+	it('should show a user wall on the ihc-tables page and render the main page', function(done) {
+		surgPathNoCookie.ihc(done);
+	});
+	it('should render the breast fixation page without login features', function(done) {
+		surgPathNoCookie.breastFixation(done);
+	});
+	it('should show a user wall on the snippets page and render the main page', function(done) {
+		surgPathNoCookie.snippets(done);
 	});
 });
