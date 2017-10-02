9a4969b1a9d6070504e3761bce15b8e4b6ea2f88
Sarah Riehl <sls.riehl@gmail.com>
2017-10-02 13:12:45 -0500

    2: add mockery email try: comment out: reinstate createTestAccount try

--- test/user.js
+++ test/user.js
@@ -34,105 +34,101 @@ describe('user pages behavior where no cookie is sent', function() {
 	});
 	//tests
 	//root
-	it('should render the main page with logged out header', function(done) {
-		root.renderIndexNoCookie(done);
+	// it('should render the main page with logged out header', function(done) {
+	// 	root.renderIndexNoCookie(done);
+	// });
+	// //signup
+	// it('should render the signup page with logged out header', function(done) {
+	// 	signupNoCookie.renderPage(done);
+	// });
+	// it('should signup with valid inputs', function(done) {
+	// 	signupNoCookie.validSignup(done);
+	// });
+	// it('should not signup with a username already taken', function(done) {
+	// 	signupNoCookie.invalidSignup(false, done);
+	// });
+	// it('should not signup with an email already taken', function(done) {
+	// 	signupNoCookie.invalidSignup(true, done);
+	// });
+	// //login
+	// it('should render the login page with logged out header', function(done) {
+	// 	loginNoCookie.renderPage(done);
+	// });
+	// it('should login with valid username and password', function(done) {
+	// 	loginNoCookie.validLogin(false, done);
+	// });
+	// it('should login with valid email and password', function(done) {
+	// 	loginNoCookie.validLogin(true, done);
+	// });
+	// it('should not login with invalid email', function(done) {
+	// 	loginNoCookie.invalidLoginCred(true, done);
+	// });
+	// it('should not login with invalid username', function(done) {
+	// 	loginNoCookie.invalidLoginCred(false, done);
+	// });
+	// it('should not login with valid email but invalid password', function(done) {
+	// 	loginNoCookie.invalidLoginPwd(true, done);
+	// });
+	// it('should not login with valid username but invalid password', function(done) {
+	// 	loginNoCookie.invalidLoginPwd(false, done);
+	// });
+	// it('should not login by email when the user is deleted', function(done) {
+	// 	loginNoCookie.deletedUser(true, done);
+	// });
+	// it('should not login by username when the user is deleted', function(done) {
+	// 	loginNoCookie.deletedUser(false, done);
+	// });
+	// //mail
+	// it('should render the mail page with logged out header', function(done) {
+	// 	mail.renderPageNoCookie(done);
+	// });
+	// it('should send mail when name, email, and message are present');
+	// //reset password: sending reset email
+	// it('should render reset password email/username input on request', function(done) {
+	// 	reset.renderPage(done);
+	// });
+	// it('should reset the password on login if require reset is true');
+	it('should send reset email with valid token on reset request submit with valid email', function(done) {
+		mail.sendResetNotReq(true, done);
 	});
-	//signup
-	it('should render the signup page with logged out header', function(done) {
-		signupNoCookie.renderPage(done);
+	it('should send reset email with valid token on reset request submit with valid username', function(done) {
+		mail.sendResetNotReq(false, done);
 	});
-	it('should signup with valid inputs', function(done) {
-		signupNoCookie.validSignup(done);
-	});
-	it('should not signup with a username already taken', function(done) {
-		signupNoCookie.invalidSignup(false, done);
-	});
-	it('should not signup with an email already taken', function(done) {
-		signupNoCookie.invalidSignup(true, done);
-	});
-	//login
-	it('should render the login page with logged out header', function(done) {
-		loginNoCookie.renderPage(done);
-	});
-	it('should login with valid username and password', function(done) {
-		loginNoCookie.validLogin(false, done);
-	});
-	it('should login with valid email and password', function(done) {
-		loginNoCookie.validLogin(true, done);
-	});
-	it('should not login with invalid email', function(done) {
-		loginNoCookie.invalidLoginCred(true, done);
-	});
-	it('should not login with invalid username', function(done) {
-		loginNoCookie.invalidLoginCred(false, done);
-	});
-	it('should not login with valid email but invalid password', function(done) {
-		loginNoCookie.invalidLoginPwd(true, done);
-	});
-	it('should not login with valid username but invalid password', function(done) {
-		loginNoCookie.invalidLoginPwd(false, done);
-	});
-	it('should not login by email when the user is deleted', function(done) {
-		loginNoCookie.deletedUser(true, done);
-	});
-	it('should not login by username when the user is deleted', function(done) {
-		loginNoCookie.deletedUser(false, done);
-	});
-	//mail
-	it('should render the mail page with logged out header', function(done) {
-		mail.renderPageNoCookie(done);
-	});
-	it('should send mail when name, email, and message are present');
-	//reset password: sending reset email
-	it('should render reset password email/username input on request', function(done) {
-		reset.renderPage(done);
-	});
-	it('should reset the password on login if require reset is true');
-	it('should send reset email with valid token on reset request submit with valid email'//, function(done) {
-	//	//not passing because message isn't loading in dom
-	// 	mail.sendResetNotReq(true, done);
-	// }
-);
-	it('should send reset email with valid token on reset request submit with valid username'//, function(done) {
-	// 	//not passing because message isn't loading in dom
-	// 	mail.sendResetNotReq(false, done);
-	// }
-);
-	it('should not send reset email with invalid email'//, function(done) {
-	// 	//not passing because message isn't loading in dom
-	// 	mail.dontSendReset(true, done);
-	// }
-);
-	it('should not send reset email with invalid username'//, function(done) {
-	// 	//not passing because message isn't loading in dom
-	// 	mail.dontSendReset(false, done);
-	// }
-);
-	//reset password: resetting password
-		//will not pass yet
-	it('should not reset password if the new password matches the old hash');
-	it('should not reset the password if the token is expired', function(done) {
-		reset.badToken('expired', done);
-	});
-	it('should not reset the password if the token is used', function(done) {
-		reset.badToken('used', done);
-	});
-	it('should not reset the password if the token is marked invalid', function(done) {
-		reset.badToken('invalid', done);
-	});
-	//reset password: logging in
-	it('should automatically load the root page logged in on successful password reset with no reset required', function(done) {
-		reset.resetSuccess(false, done);
-	});
-	it('should automatically load the root page logged in on successful password reset and reset reset required', function(done) {
-		reset.resetSuccess(true, done);
-	});
-	//user settings page
-		//will not pass
-	it('should not get the user settings page');
-		//will not pass?
-	it('should post the user settings page with valid password entered');
-		//will not pass?
-	it('should login the user on valid user settings submit');
+// 	it('should not send reset email with invalid email'//, function(done) {
+// 	// 	//not passing because message isn't loading in dom
+// 	// 	mail.dontSendReset(true, done);
+// 	// }
+// );
+// 	it('should not send reset email with invalid username'//, function(done) {
+// 	// 	//not passing because message isn't loading in dom
+// 	// 	mail.dontSendReset(false, done);
+// 	// }
+// );
+// 	//reset password: resetting password
+// 		//will not pass yet
+// 	it('should not reset password if the new password matches the old hash');
+// 	it('should not reset the password if the token is expired', function(done) {
+// 		reset.badToken('expired', done);
+// 	});
+// 	it('should not reset the password if the token is used', function(done) {
+// 		reset.badToken('used', done);
+// 	});
+// 	it('should not reset the password if the token is marked invalid', function(done) {
+// 		reset.badToken('invalid', done);
+// 	});
+// 	//reset password: logging in
+// 	it('should automatically load the root page logged in on successful password reset with no reset required', function(done) {
+// 		reset.resetSuccess(false, done);
+// 	});
+// 	it('should automatically load the root page logged in on successful password reset and reset reset required', function(done) {
+// 		reset.resetSuccess(true, done);
+// 	});
+// 	//user settings page
+// 		//will not pass
+// 	it('should not get the user settings page');
+// 		//will not pass?
+// 	it('should post the user settings page with valid password entered');
+// 		//will not pass?
+// 	it('should login the user on valid user settings submit');
 
 });
