7315a44440269c2a31b75c04c7efb829b5260753
Chandra Krishnan <chandra.krishnan@gmail.com>
2017-12-10 14:47:10 -0600

    - Updated Rhabdomyosarcoma

--- public/js/cap/rhabdo-scripts.js
+++ public/js/cap/rhabdo-scripts.js
@@ -3,81 +3,98 @@ $(window).on('load', function() {
 //                        Autocomplete                         //
 // *************************************************************/
 
-    $("input#box5").autocomplete({
-        source: ['Embryonal',
-            'Alveolar',
-            'Spindle cell and sclerosing',
-            'Ectomesenchymoma',
-            'Rhabdomyosarcoma, NOS',
-            'Other:'],
-        appendTo: '#Leftpanel'
 
-    });
 
 
 //*************************************************************//
 //                        Pop-ups                              //
 // *************************************************************/
 
-    $('#box1').change(function(){
+    $('#box1').on("change", function(){
         var sel = $('#box1').val();
-        if (sel == 'Amputation') {
-            $('#box1_2').show();}
-        else {$('#box1_2').hide();}
-        if (sel == 'Other') {
-            $('#box1_3').show();}
-        else {$('#box1_3').hide();}
-    });
-
-    $('#box3').change(function () {
-        var sel = $('#box3').val();
-        if (sel == 'Other:') {
-            $('#box3_2').show();
-        } else {
-            $('#box3_2').hide();
+        if (sel.indexOf('Amputation') > -1){
+            $('#box1_2').show();
+            $('.margins').show();
+        } else if (sel.indexOf('Other') > -1){
+            $('#box1_3').show();
+        } else if (sel.indexOf('resection') >-1){
+            $('.margins').show();
         }
-    });
-
-    $('#box5').blur(function () {
-        var sel = $('#box5').val();
-        if (sel == 'Other:') {
-            $('#box5_2').show();
-        } else {
-            $('#5_2').hide();
-        }
-    });
-
-    $('#box7_5').change(function(){
-        var sel = $('#box7_5').val();
-        if (sel == 'Other') {
-            $('#box7_5_2').show();}
         else {
-            $('#box7_5_2').hide();}
-    });
+            $('#box1_3').hide();
+            $('#box1_2').hide();
+            $('.margins').hide();
 
-    $('#box8').change(function () {
-        var sel = $('#box8').val();
-        if (sel == 'Sarcoma involvement of margins not identified') {
-            $('#box8_2').show();
-            $('#box8_3').hide();
-        }
-        else if (sel == 'Margin(s) involved by sarcoma') {
-            $('#box8_3').show();
-            $('#box8_2').hide();
-        }
-        else if (sel == 'Cannot be assessed' || 'Indeterminate') {
-            $('#box8_2').hide();
-            $('#box8_3').hide();
         }
     });
 
-    $('#box9').change(function () {
-        var sel = $('#box9').val();
-        if (sel == 'Metastatic involvement of regional lymph nodes not identified' || sel == 'Regional lymph node metastasis present') {
-            $('#lymphnodes').show();
-        } else if (sel == 'No regional lymph nodes sampled' || sel == 'No nodes submitted or found'){
-            $('#lymphnodes').hide();
+    $('#box3').on("change", function(){
+        var sel = $('#box3').val();
+        if (sel.indexOf("Other") > -1) {
+
+            $('#box3_2').show();}
+        else {
+            $('#box3_2').hide();}
+    });
+
+    $('#box5').on("change", function(){
+        var sel = $('#box5').val();
+        if (sel.indexOf("Other") > -1) {
+
+            $('#box5_2').show();}
+        else {
+            $('#box5_2').hide();}
+    });
+
+    $('#box7').on("change", function(){
+        var sel = $('#box7').val();
+        if (sel.indexOf("Other") > -1) {
+
+            $('#box7_2').show();}
+        else {
+            $('#box7_2').hide();}
+    });
+
+    $('#box8').on("change", function(){
+        var sela = $('#box8').val();
+        if (sela.indexOf('Uninvolved') > -1){
+            $('#box8_1').show();
+            $('#box8_2').show();}
+        else {$('#box8_1').hide();
+            $('#box8_2').hide();}
+        if (sela.indexOf("Involved") > -1) {
+            $('#box8_3').show();}
+        else {$('#box8_3').hide();}
+    });
+
+    $("#box13").on("change", function(){
+        if ($(this).is(":checked")){
+            $(".lnchk").show();
         }
+        else{
+            $(".lnchk").hide();
+        }
+    });
+
+    $('#box12').on("change", function(){
+        var sel = $('#box12').val();
+        if (sel.indexOf("Present") > -1) {
+
+            $('#box12_2').show();}
+        else {
+            $('#box12_2').hide();}
+    });
+
+
+
+
+    $('#box16').on("change", function(){
+        var sel = $('#box16').val();
+        if (sel.indexOf("Present") > -1) {
+
+            $('#box16_2').show();}
+        else {
+            $('#box16_2').hide();}
     });
 
 
@@ -88,84 +105,127 @@ $(window).on('load', function() {
 // *************************************************************/
     $('.writeReport').on('click', function () {
 
-        $('input[type="text"]').each(function () {
-            if ($(this).val().length < 1) {
-                $(this).val($(this).attr('placeholder'));
-            }
-            if ($(this).val().length < 1) {
-                $(this).addClass('empty')
+    // ***************** INPUT VALIDATION ********************//
+        $('select[multiple]:visible').each(function () {
+            // Check if at least one selection is made
+            if ($(this).val().length > 0) {
+                $(this).removeClass('empty');
+            } else {
+                $(this).addClass('empty');
+                $('#cap-valid').show();
             }
         });
 
-        var captext = "Rhabdomyosarcoma Cancer Synoptic\nAJCC 2018 cancer staging version\n\n";
+        $('input[type="text"]:visible').each(function () {
+            // Check if at least one selection is made
+            if ($.trim($(this).val()).length > 0) {
+                $(this).removeClass('empty');
+            } else {
+                    $(this).addClass('empty');
+                    $('#cap-valid').show();
+                }
+        });
+
+        // ***************** END VALIDATION ********************//
+
+
+
+        var captext = "Rhabdomyosarcoma Cancer Synoptic\n\n";
+
         var box_1 = $("#box1").val();
         var box_1_2 = $("#box1_2").val();
         var box_1_3 = $("#box1_3").val();
-        if (box_1 == 'Amputation') { captext += "\nProcedure:\n- "+box_1+": "+box_1_2+"\n";}
-        else if (box_1 == 'Other'){ captext += "\nProcedure:\n- "+box_1+": "+box_1_3+"\n";}
+        if (box_1.indexOf("Amputation") > -1) {
+            captext += "\nProcedure:\n- "+box_1+"\n- "+box_1_2+"\n";}
+        else if (box_1.indexOf("Other") > -1) {
+            captext += "\nProcedure:\n- "+box_1+"\n- "+box_1_3+"\n";}
         else {captext += "\nProcedure:\n- "+box_1+"\n";}
 
-        var box_2 = $("#box2").val();
-        if(box_2 != "Not applicable"){
-            captext += "\n+ Preoperative treatment:\n- "  + box_2+ "\n";}
-
         var box_3 = $("#box3").val();
-        if (box_3 == 'Other:'){
-            box_3_2 = $("#box3_2").val();
-            captext += "\nTumor Site:\n- "+box_3_2+"\n";}
-        else {captext += "\nTumor Site:\n- "+box_3+"\n";}
+        var box_3_2 = $("#box3_2").val();
+        if (box_3.indexOf("Other") > -1) {
+            captext += "\nTumor Site:\n- "  + box_3_2+ "\n";}
+        else {captext += "\nTumor Site:\n- "  + box_3+ "\n";}
+
 
         var box_4 = $("#box4").val();
-        captext += "\nTumor size:\n- "  + box_4.replace(/cm/,'') + "cm\n";
+        captext += "\nTumor Size:\n- "  + box_4.replace(/cm/,'') + "cm\n";
 
         var box_5 = $("#box5").val();
-        if (box_5 == 'Other:'){
-            box_5_2 = $("#box5_2").val();
-            captext += "\nHistologic Type:\n- "+box_5_2+"\n";}
-        else {captext += "\nHistologic Type:\n- "+box_5+"\n";}
+        var box_5_2 = $("#box5_2").val();
+        if (box_5.indexOf("Other") > -1) {
+            captext += "\nHistologic Type:\n- "  + box_5_2+ "\n";}
+        else {captext += "\nHistologic Type:\n- "  + box_5+ "\n";}
+
+
+        var box_6 = $("#box6").val();
+        if(box_6 != "Not applicable"){
+            captext += "\nAnaplasia:\n- "  + box_6+ "\n";}
 
         var box_7 = $("#box7").val();
-        captext += "\nAnaplasia:\n- "+box_7+"\n";
+        var box_7_2 = $("#box7_2").val();
+        if (box_7.indexOf("Other") > -1) {
+            captext += "\nFusion Status:\n- "  + box_7_2+ "\n";}
+        else {captext += "\nFusion Status:\n- "  + box_7+ "\n";}
 
-        var box_7_5 = $("#box7_5").val();
-        var box_7_5_2 = $("#box7_5_2").val();
-        if (box_7_5 == 'Other'){
-            captext += "\nFusion Status:\n- "  + box_7_5_2+ "\n";}
-        else {captext += "\nFusion Status:\n- "  + box_7_5+ "\n";}
 
         var box_8 = $("#box8").val();
-        if (box_8 == 'Sarcoma involvement of margins not identified'){
-            box_8_2 = $("#box8_2").val();
-            captext += "\nMargins:\n- "+box_8+"\n- Distance of sarcoma to closest margin: "+box_8_2+"cm\n";}
-        else if (box_8== 'Margin(s) involved by sarcoma'){
-            box_8_3 = $("#box8_3").val();
-            captext += "\nMargins:\n- "+box_8+": "+box_8_3+" margin\n";}
-        else {captext += "\nMargins:\n- "+box_8+"\n";}
-
-        var box_9 = $("#box9").val();
-        var box_10 = $("#box10").val();
-        var box_11 = $("#box11").val();
-
-        captext += "\nLymph Nodes:\n- "+box_9+"\n";
-
-        if (box_9 == 'Metastatic involvement of regional lymph nodes not identified' || box_9 == 'Regional lymph node metastasis present') {
-            if (parseInt(box_11, 10) > parseInt(box_10,10)){alert ('Lymph node count error, please check your numbers'); $('#box10').focus(); return;}
-            captext += "\nNumber of Lymph Nodes Examined:\n- "+box_10+"\n";
-            captext += "\nNumber of Lymph Nodes Involved:\n- "+box_11+"\n";}
+        var box_8_1 = $("#box8_1").val();
+        var box_8_2 = $("#box8_2").val();
+        var box_8_3 = $("#box8_3").val();
+        if (box_8 != "Not applicable") {
+            if (box_8.indexOf("Uninvolved") > -1) {
+                captext += "\nMargins (for resections only):\n- " + box_8 + "\n- Nearest margin: " + box_8_1 + "\n- Distance to this margin: " + box_8_2.replace(/mm/, "") + "mm\n";
+            }
+            else if (box_8.indexOf("Involved") > -1) {
+                captext += "\nMargins (for resections only):\n- " + box_8 + "\n- Margin involved: " + box_8_3 + "\n";
+            }
+            else {
+                captext += "\nMargins (for resections only):\n- " + box_8 + "\n";
+            }
+        }
 
         var box_12 = $("#box12").val();
-        if (box_12 = ' '){
-            captext += "\nDistant Metastasis:\n- Not applicable\n";}
-        else {captext += "\nDistant Metastasis:\n- "+box_12+"\n";}
+        var box_12_2 = $("#box12_2").val();
+        if (box_12.indexOf("Present") > -1) {
+            captext += "\nDistant Metastasis:\n- "  + box_12_2+ "\n";}
+        else {captext += "\nDistant Metastasis:\n- "  + box_12+ "\n";}
 
-        var box_13 = $("#box13").val();
-        captext += "\nThe Intergroup Rhabdomyosarcoma Study Postsurgical Clinical Grouping System:\n- "+box_13+"\n";
+
+        if ($("#box13").is(':checked')) {
+            var box_14 = $("#box14").val();
+            var box_15 = $("#box15").val();
+            captext += "\nLymph nodes:\n\tLymph Nodes Examined: "+box_14+"\n\tLymph nodes involved: "+box_15+"\n";} else {
+            captext += "\nLymph nodes: None submitted\n";}
+
+
+        var box_2 = $("#box2").val();
+        captext += "\n+ Preoperative Treatment:\n- "  + box_2+ "\n";
+
+
+        var box_16 = $("#box16").val();
+        var box_16_2 = $("#box16_2").val();
+        if (box_16 != "Not applicable") {
+            if (box_16.indexOf("Present") > -1) {
+                captext += "\n+ Treatment Effect:\n- Present: " + box_16_2.replace(/%/, '') + "% necrosis\n";
+            }
+            else {
+                captext += "\n+ Treatment Effect:\n- " + box_16 + "\n";
+            }
+        }
+
+
+        var box_17 = $("#box17").val();
+        captext += "\nThe Intergroup Rhabdomyosarcoma Study Postsurgical Clinical Grouping System:\n- "  + box_17+ "\n";
+
 
 
 
         $('#outPut-1').val(captext);
 
-				dataObj.singleSection = $('#outPut-1').val();
-				makeCreatePdfBtn();
+        dataObj.singleSection = $('#outPut-1').val();
+        makeCreatePdfBtn();
     });
 });
+
+
