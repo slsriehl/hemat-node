1fe1ae748c983c24ac1b1be9a826bd92d624fd06
Chandra Krishnan <chandra.krishnan@gmail.com>
2017-12-18 22:24:09 -0600

    - Created Larynx tumors

--- routes/pages/cap.js
+++ routes/pages/cap.js
@@ -170,6 +170,12 @@ router.get('/cap/melanoma-bx', (req, res) => {
 ]);
 });
 
+router.get('/cap/larynx-t', (req, res) => {
+    controller.userWall(req, res, './page-views/cap/larynx-t.hbs', [
+    "/js/cap/larynx-scripts.js",
+]);
+});
+
 router.get('/cap/pharynx-t', (req, res) => {
     controller.userWall(req, res, './page-views/cap/pharynx-t.hbs', [
     "/js/cap/pharynx-scripts.js",
