b53b2ca87ee1a9356c3a2f84e370333ad8630c59
Chandra Krishnan <chandra.krishnan@gmail.com>
2017-12-25 15:59:40 -0600

    - Updated Adult renal tumors

--- public/js/cap/kidney-scripts.js
+++ public/js/cap/kidney-scripts.js
@@ -10,76 +10,125 @@ $(window).on('load', function() {
 //                        Pop-ups                              //
 // *************************************************************/
 
-    $("#box18").change(function () {
-        $('#box18_2').toggle();
-        $('#box18_3').toggle();
-    });
-
-    $("#box19").change(function () {
-        var box_19 = $("#box19").val();
-        if (box_19 == 'Pathologic findings present'){
-            $('.normalkidney').show();
-        } else
-            $('.normalkidney').hide();
-
-    });
-
-    $('#box1').change(function(){
+    $('#box1').on("change", function(){
         var sel = $('#box1').val();
-        if (sel == 'Other:') {
+        if (sel.indexOf("Other") > -1) {
+
             $('#box1_2').show();}
-        else {$('#box1_2').hide();}
+        else {
+            $('#box1_2').hide();}
     });
 
-    $('#box3').change(function(){
+    $('#box3').on("change", function(){
         var sel = $('#box3').val();
-        if ($.inArray('Other:', sel) >-1) {
+        if ($.inArray('Other', sel) >-1) {
             $('#box3_2').show();}
         else {$('#box3_2').hide();}
     });
 
-    $('#box6').change(function(){
+    $('#box6').on("change", function(){
         var sel = $('#box6').val();
-        if ($.inArray('Tumor extension into other organ(s);', sel) >-1) {
+        if (sel.indexOf("Other") > -1) {
+
             $('#box6_2').show();}
-        else {$('#box6_2').hide();}
+        else {
+            $('#box6_2').hide();}
     });
 
-
-    $('#box7').change(function(){
+    $('#box7').on("change", function(){
         var sel = $('#box7').val();
-        if (sel == 'Other:') {
+        if (sel.indexOf("Present") > -1) {
+
             $('#box7_2').show();}
-        else {$('#box7_2').hide();}
+        else {
+            $('#box7_2').hide();}
     });
 
-    $('#box8').change(function(){
-        var sel = $('#box8').val();
-        if (sel == 'Present;') {
-            $('#box8_2').show();}
-        else {$('#box8_2').hide();}
-    });
-
-    $('#box10').change(function(){
+    $('#box10').on("change", function(){
         var sel = $('#box10').val();
-        if (sel == 'Other:') {
+        if (sel.indexOf("Present") > -1) {
+
             $('#box10_2').show();}
-        else {$('#box10_2').hide();}
+        else {
+            $('#box10_2').hide();}
     });
 
-    $('#box11').change(function(){
+    $('#box11').on("change", function(){
         var sel = $('#box11').val();
-        if ($.inArray('Tumor extension into other organ(s);', sel) >-1) {
+        if (sel.indexOf('structures') >-1) {
             $('#box11_2').show();}
         else {$('#box11_2').hide();}
     });
 
-    $('#box12').change(function(){
+    $('#box12').on("change", function(){
         var sel = $('#box12').val();
-        if ($.inArray('Other:', sel) > -1) {
+        if (sel.indexOf("Involved") > -1) {
+
             $('#box12_2').show();}
-        else {$('#box12_2').hide();}
+        else {
+            $('#box12_2').hide();}
     });
+
+    $('#box50').on("change", function(){
+        var sel = $('#box50').val();
+        if ($.inArray('Other', sel) >-1) {
+            $('#box50_2').show();}
+        else {$('#box50_2').hide();}
+    });
+
+    $("#box18").on("change", function(){
+        if ($(this).is(":checked")){
+            $(".lnchk").show();
+        }
+        else{
+            $(".lnchk").hide();
+        }
+    });
+
+    $("#box17").on("change", function(){
+        var sel = $("#box17").val();
+        if (sel != "pMx"){
+            $("#box17_2").show();
+        } else {
+            $("#box17_2").hide();
+        }
+    });
+
+    $("#box20").on('input', function(){
+        var sel = $(this).val();
+        if (sel > 0){
+            $(".posnodes").show();
+        } else {
+            $(".posnodes").hide();
+        }
+    });
+
+
+
+    $('#box22').on("change", function(){
+        var sela = $('#box22').val();
+        var trig1 = sela.filter(el => el.indexOf('Glomerular') > -1);
+        var trig2 = sela.filter(el => el.indexOf('Tubulointerstitial') > -1);
+        var trig3 = sela.filter(el => el.indexOf('Vascular') > -1);
+        var trig4 = sela.filter(el => el.indexOf('Other') > -1);
+        if (trig1.length > 0) {
+            $('#box22_2').show();}
+        else {$('#box22_2').hide();
+        }
+        if (trig2.length > 0) {
+            $('#box22_3').show();}
+        else {$('#box22_3').hide();}
+        if (trig3.length > 0) {
+            $('#box22_4').show();}
+        else {$('#box22_4').hide();}
+        if (trig4.length > 0) {
+            $('#box22_5').show();}
+        else {$('#box22_5').hide();}
+
+
+    });
+
+
 
 
 //************************************************************//
@@ -87,124 +136,167 @@ $(window).on('load', function() {
 // *************************************************************/
     $('.writeReport').on('click', function () {
 
-        $('input[type="text"]').each(function () {
-            if ($(this).val().length < 1) {
-                $(this).val($(this).attr('placeholder'));
-            }
-            if ($(this).val().length < 1) {
-                $(this).addClass('empty')
+    // ***************** INPUT VALIDATION ********************//
+        $('select[multiple]:visible').each(function () {
+            // Check if at least one selection is made
+            if ($(this).val().length > 0) {
+                $(this).removeClass('empty');
+            } else {
+                $(this).addClass('empty');
+                $('#cap-valid').show();
             }
         });
 
-        var captext = "Kidney Cancer Synoptic\nAJCC 2018 cancer staging version\n\n\n";
+        $('input[type="text"]:visible').each(function () {
+            // Check if at least one selection is made
+            if ($.trim($(this).val()).length > 0) {
+                $(this).removeClass('empty');
+            } else {
+                    $(this).addClass('empty');
+                    $('#cap-valid').show();
+                }
+        });
+
+        // ***************** END VALIDATION ********************//
+
+
+
+        var captext = "Renal Cancer Synoptic\n(pTNM requirements from the 8th Edition, AJCC Staging Manual)\n\n";
 
         var box_1 = $("#box1").val();
         var box_1_2 = $("#box1_2").val();
-        if (box_1 == 'Other:'){
+        if (box_1.indexOf("Other") > -1) {
             captext += "\nProcedure:\n- "  + box_1_2+ "\n";}
         else {captext += "\nProcedure:\n- "  + box_1+ "\n";}
+
 
         var box_2 = $("#box2").val();
         captext += "\nSpecimen Laterality:\n- "  + box_2+ "\n";
 
         var box_3 = $("#box3").val();
         var box_3_2 = $("#box3_2").val();
-        if ($.inArray('Other:', box_3) >-1){
-            captext += "\nTumor Site:\n- "  + box_3.join('\n- ').replace(/Other:/, box_3_2) + "\n";}
+        if ($.inArray('Other', box_3) >-1){
+            captext += "\n+ Tumor Site:\n- "  + box_3.join(', ').replace(/Other/, box_3_2) + "\n";}
+        else {captext += "\n+ Tumor Site:\n- "  + box_3.join(', ') + "\n";}
 
         var box_4 = $("#box4").val();
-        captext += "\nTumor Size:\n- "  + box_4 + "\n";
+        captext += "\nTumor Size:\n- "  + box_4.replace(/cm/,'') + "cm\n";
 
         var box_5 = $("#box5").val();
         captext += "\nTumor Focality:\n- "  + box_5+ "\n";
 
         var box_6 = $("#box6").val();
         var box_6_2 = $("#box6_2").val();
-        if ($.inArray('Tumor extension into other organ(s);', box_6) >-1){
-            captext += "\nMacroscopic Extent of Tumor:\n- "  + box_6.join('\n- ').replace(/;/, ': '+box_6_2) + "\n";}
-        else {captext += "\nMacroscopic Extent of Tumor:\n- "  + box_6.join('\n- ') + "\n";}
+        if (box_6.indexOf("Other") > -1) {
+            captext += "\nHistologic Type:\n- "  + box_6_2+ "\n";}
+        else {captext += "\nHistologic Type:\n- "  + box_6+ "\n";}
+
 
         var box_7 = $("#box7").val();
         var box_7_2 = $("#box7_2").val();
-        if (box_7 == 'Other:'){
-            captext += "\nHistologic Type:\n- "  + box_7_2+ "\n";}
-        else {captext += "\nHistologic Type:\n- "  + box_7+ "\n";}
+        if (box_7.indexOf("Present") > -1) {
+            captext += "\nSarcomatoid Features:\n- Present: "  + box_7_2+ "%\n";}
+        else {captext += "\nSarcomatoid Features:\n- "  + box_7+ "\n";}
+
 
         var box_8 = $("#box8").val();
-        var box_8_2 = $("#box8_2").val();
-        if (box_8 == 'Present;'){
-            captext += "\nSarcomatoid Features:\n- "  + box_8.replace(/;/, ', '+box_8_2)+ "% of tumor\n";}
-        else {captext += "\nSarcomatoid Features:\n- "  + box_8+ "\n";}
+        captext += "\nRhabdoid Features:\n- "  + box_8+ "\n";
 
         var box_9 = $("#box9").val();
-        captext += "\nTumor Necrosis:\n- "  + box_9+ "\n";
+        captext += "\nHistologic Grade:\n- "  + box_9+ "\n";
 
         var box_10 = $("#box10").val();
         var box_10_2 = $("#box10_2").val();
-        if (box_10 == 'Other:'){
-            captext += "\nHistologic Grade (Fuhrman Nuclear Grade):\n- "  + box_10_2+ "\n";}
-        else {captext += "\nHistologic Grade (Fuhrman Nuclear Grade):\n- "  + box_10+ "\n";}
+        if (box_10.indexOf("Present") > -1) {
+            captext += "\nTumor Necrosis:\n- Present: "  + box_10_2+ "%\n";}
+        else {captext += "\nTumor Necrosis:\n- "  + box_10+ "\n";}
+
 
         var box_11 = $("#box11").val();
         var box_11_2 = $("#box11_2").val();
-        if ($.inArray('Tumor extension into other organ(s);', box_11) >-1){
-            captext += "\nMicroscopic Tumor Extension:\n- "  + box_11.join('\n- ').replace(/;/, ', '+box_11_2) + "\n";}
-        else {captext += "\nMicroscopic Tumor Extension:\n- "  + box_11.join('\n- ') + "\n";}
+        if (box_11.indexOf('structures') >-1){
+            captext += "\nTumor Extension:\n- "  + box_11.replace(/structures/, "structures, including: " + box_11_2) + "\n";}
+        else {captext += "\nTumor Extension:\n- "  + box_11 + "\n";}
 
         var box_12 = $("#box12").val();
-        var box_12_2 = $("#box12_2").val();
-        if ($.inArray('Other:', box_12) > -1){
-            captext += "\nMargins:\n- "  + box_12.join('\n- ').replace(/Other:/, box_12_2)+ "\n";}
-        else {captext += "\nMargins:\n- "  + box_12.join('\n- ')+ "\n";}
+        if (box_12.indexOf("Involved") > -1) {
+            captext += "\nMargins:\n- "  +box_12+"\n";
+            var box_50 = $("#box50").val();
+            var box_50_2 = $("#box50_2").val();
+            if ($.inArray('Other', box_50) >-1){
+                captext += "\t- "  + box_50.join('\n\t- ').replace(/Other/, box_50_2) + "\n";}
+            else {captext += "\t- "  + box_50.join('\n\t- ') + "\n";}
+
+        }
+        else {captext += "\nMargins:\n- "  + box_12+ "\n";}
+
+
+
 
         var box_13 = $("#box13").val();
-        captext += "\n+ Lymph-Vascular Invasion:\n- "  + box_13+ "\n";
+        captext += "\n+ Lymphovascular Invasion:\n- "  + box_13+ "\n";
 
         var box_14 = $("#box14").val();
         var box_15 = $("#box15").val();
         var box_16 = $("#box16").val();
         var box_17 = $("#box17").val();
+        var box_17_2 = $("#box17_2").val();
         captext += '\nPathologic Staging (pTNM):\n- ';
-        if (box_14 != "Not applicable"){captext += box_14.join("")+' '+box_15+" "+box_16+" "+box_17+"\n";}
-        else {captext += box_15+" "+box_16+" "+box_17+"\n";}
-
+        if (box_14 != "Not applicable"){
+            if (box_17 != "pMx"){
+                captext += box_14.join("")+" "+box_15+" "+box_16+" "+box_17+" (metastatic site(s): " + box_17_2 + ")\n";
+            } else {
+                captext += box_14.join("")+" "+box_15+" "+box_16+" "+box_17+"\n";
+            }
+        } else {
+            if (box_17 != "pMx"){
+                captext += box_15+" "+box_16+" "+box_17+" (metastatic site(s): " + box_17_2 + ")\n";
+            } else {
+                captext += box_15+" "+box_16+" "+box_17+"\n";
+            }
+        }
         if ($("#box18").is(':checked')) {
-            var box_18_2 = $("#box18_2").val();
-            var box_18_3 = $("#box18_3").val();
-            captext += "\nLymph nodes examined: "+box_18_2+"\nLymph nodes involved: "+box_18_3+"\n";}
+            var box_19 = $("#box19").val();
+            var box_20 = $("#box20").val();
+            captext += "\nLymph nodes:\n\tLymph Nodes Examined: "+box_19+"\n\tLymph nodes involved: "+box_20+"\n";} else {
+            captext += "\nLymph nodes: None submitted\n";}
 
-        var box_19 = $("#box19").val();
-        var box_19_2 = $("#box19_2").val();
-        var box_19_3 = $("#box19_3").val();
-        var box_19_4 = $("#box19_4").val();
-        var box_19_5 = $("#box19_5").val();
+        var box_21 = $("#box21").val();
+        captext += "\n+ Size of Largest Metastatic Deposit:\n- "  + box_21.replace(/cm/,'') + "cm\n";
 
-        if (box_19 != 'Pathologic findings present'){
-            captext += "\nPathologic Findings in Nonneoplastic Kidney:\n- "  + box_19+ "\n";}
+        var box_22 = $("#box22").val();
+        var box_22_2 = $("#box22_2").val();
+        var box_22_3 = $("#box22_3").val();
+        var box_22_4 = $("#box22_4").val();
+        var box_22_5 = $("#box22_5").val();
+        var trig1_box_22 = box_22.filter(el => el.indexOf("Glomerular") > -1);
+        var trig2_box_22 = box_22.filter(el => el.indexOf("Tubulointerstitial") > -1);
+        var trig3_box_22 = box_22.filter(el => el.indexOf("Vascular") > -1);
+        var trig4_box_22 = box_22.filter(el => el.indexOf("Other") > -1);
+        captext += "\nPathologic Findings in Nonneoplastic Kidney:\n";
+        if (trig1_box_22.length > 0){
+            captext += "- Glomerular disease: "+box_22_2+"\n";
+        }
+        if (trig2_box_22.length > 0){
+            captext += "- Tubulointerstitial disease: "+box_22_3+"\n";
+        }
+        if (trig3_box_22.length > 0){
+            captext += "- Vascular disease: "+box_22_4+"\n";
+        }
+        if (trig4_box_22.length > 0){
+            captext += "- Other disease: "+box_22_5+"\n";
+        }
 
-        if (box_19 == 'Pathologic findings present'){
-            captext += "\nPathologic Findings in Nonneoplastic Kidney:\n";}
-        if (box_19_2 != 'specify type'){
-            captext += "\n  Glomerular disease: "  + box_19_2 ;}
-        if (box_19_3 != 'specify type'){
-            captext += "\n  Tubulointerstitial disease: "  + box_19_3 ;}
-        if (box_19_4 != 'specify type'){
-            captext += "\n  Vascular disease: "  + box_19_4 ;}
-        if (box_19_5 != 'specify type'){
-            captext += "\n  Other: "  + box_19_5 ;}
+        else {captext += "- "+box_22.join("\n- ")+"\n";}
 
-        var box_20 = $("#box20").val();
-        if (box_20 != 'Not applicable'){
-            captext += "\n+  Other Tumors and/or Tumor-like Lesions:\n- "  + box_20 + "\n";}
+
 
 
         $('#outPut-1').val(captext);
 
-				dataObj.singleSection = $('#outPut-1').val();
-				makeCreatePdfBtn();
-
+        dataObj.singleSection = $('#outPut-1').val();
+        makeCreatePdfBtn();
     });
 });
-/**
- * Created by Chandra Krishnan on 8/16/2017.
- */
+
+
