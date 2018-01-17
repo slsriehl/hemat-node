51f73fde59f353281d218a6c473bdc0df4bf604e
Chandra Krishnan <chandra.krishnan@gmail.com>
2018-01-16 22:49:35 -0600

    - Fixed optional selections and other small UI/JS bugs

--- public/js/cap/testis-scripts.js
+++ public/js/cap/testis-scripts.js
@@ -119,56 +119,12 @@ $(window).on('load', function() {
         });
     });
 
-
-// strings and compares them, returning -1 if `a` comes before `b`, 0 if `a`
-    // and `b` are equal, and 1 if `a` comes after `b`.
-    cmpStrNum = function(a, b) {
-        // Regular expression to separate the digit string from the non-digit strings.
-        var reParts = /\d+|\D+/g;
-
-        // Regular expression to test if the string has a digit.
-        var reDigit = /\d/;
-
-        // Get rid of casing issues.
-        a = a.toUpperCase();
-        b = b.toUpperCase();
-
-        // Separates the strings into substrings that have only digits and those
-        // that have no digits.
-        var aParts = a.match(reParts);
-        var bParts = b.match(reParts);
-
-        // Used to determine if aPart and bPart are digits.
-        var isDigitPart;
-
-        // If `a` and `b` are strings with substring parts that match...
-        if(aParts && bParts &&
-            (isDigitPart = reDigit.test(aParts[0])) == reDigit.test(bParts[0])) {
-            // Loop through each substring part to compare the overall strings.
-            var len = Math.min(aParts.length, bParts.length);
-            for(var i = 0; i < len; i++) {
-                var aPart = aParts[i];
-                var bPart = bParts[i];
-
-                // If comparing digits, convert them to numbers (assuming base 10).
-                if(isDigitPart) {
-                    aPart = parseInt(aPart, 10);
-                    bPart = parseInt(bPart, 10);
-                }
-
-                // If the substrings aren't equal, return either -1 or 1.
-                if(aPart != bPart) {
-                    return aPart < bPart ? -1 : 1;
-                }
-
-                // Toggle the value of isDigitPart since the parts will alternate.
-                isDigitPart = !isDigitPart;
-            }
-        }
-
-        // Use normal comparison.
-        return (a <= b) - (a >= b) ;
-    };
+// Sort by Percentages
+    function sortPct( a,b ) {
+        a = parseInt(a);
+        b = parseInt(b);
+        return b-a;
+    }
 
 //*************************************************************//
 //                        Pop-ups                              //
@@ -330,13 +286,13 @@ $(window).on('load', function() {
         if (box_5_6.length > 0){
             mgct.push(box_5_6+"% teratoma");
         }
-        mgct.sort(cmpStrNum);
+        mgct.sort(sortPct);
         console.log(mgct);
         var box_5_7 = $("#box5_7").val();
         var box_5_8 = $("#box5_8").val();
         var box_5_9 = $("#box5_9").val();
         if(box_5.indexOf('Mixed germ cell tumor') > -1){
-            captext += '\nHistologic Type:\n- ' + box_5 + ': ' + box_5_2 + '% Seminoma, ' + box_5_3 + '% Yolk Sac, ' + box_5_4 + '% Choriocarcinoma, ' + box_5_5 + '% Embryonal carcinoma, ' + box_5_6 + '% Teratoma\n';
+            captext += '\nHistologic Type:\n- ' + box_5 + ', with:\n\t' + mgct.join("\n\t")+'\n';
         }
         else if (box_5.indexOf('Teratoma with') > -1) {
             captext += "\nHistologic Type:\n- " + box_5 + " " + box_5_7 + "\n";
