3c2c33cffe3048dba13ecb021bf22d960ba4b1e5
Chandra Krishnan <chandra.krishnan@gmail.com>
2019-06-22 21:57:03 -0500

    Fixed DB connection

--- routes/pages/plac-ref.js
+++ routes/pages/plac-ref.js
@@ -5,20 +5,13 @@ const router = new express.Router;
 let mysql  = require('mysql');
 
 console.log(process.env.DB_TABLE);
-/*
+
 var config = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_TABLE
 };
-*/
-var config = {
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "hemat"
-}
 
 let connection = mysql.createConnection(config);
 
