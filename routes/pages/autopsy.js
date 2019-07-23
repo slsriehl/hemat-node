ee9a3abdf9a0759d312e5b5482a4354929164c2a
Sarah Schieffer Riehl <sls.riehl@gmail.com>
2019-07-22 23:29:07 -0500

    autopsy path edit correction

--- routes/pages/autopsy.js
+++ routes/pages/autopsy.js
@@ -4,14 +4,14 @@ const router = new express.Router;
 
 const controller = require('../../controllers/pages');
 
-router.get('/autopsy/weights', (req, res) => {
+router.get('/weights', (req, res) => {
 	//not behind a login
 	controller.openAccess(req, res, './page-views/autopsy/autopsy-weights.hbs', [
         "/js/autopsy/autopsy-weights-scripts.js"
 	]);
 });
 
-router.get('/autopsy/autopsy-pedi', (req, res) => {
+router.get('/autopsy-pedi', (req, res) => {
     //not behind a login
     controller.openAccess(req, res, './page-views/autopsy/autopsy-pedi.hbs', [
         "/js/autopsy/autopsy-pedi-scripts.js"
