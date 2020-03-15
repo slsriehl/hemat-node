8960e6ba30bcc26ce2748dcd2c2cd31496cfa840
Sarah Schieffer Riehl <sls.riehl@gmail.com>
2020-03-15 00:04:45 -0500

    server start tagline now declares environment

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = require('./app');
 // ++++++ SERVER LISTEN ++++++
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, function () {
-  console.log('express server listening to your mom at port ' + PORT);
-});
+const ENV = process.env.NODE_ENV ? process.env.NODE_ENV.toUpperCase() : undefined;
+
+if(ENV) {
+  const server = app.listen(PORT, () => console.log(`${ENV} express server listening to your mom at port ${PORT}`));
+
+} else {
+  console.log(`The server didn't start because you've got problems: no NODE_ENV is set.  Things will get wonky.`);
+  
+}
