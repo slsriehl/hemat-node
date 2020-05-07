e945b4eb8572f63797a9b99fb5834d69740cbf02
Sarah Schieffer Riehl <sls.riehl@gmail.com>
2020-05-06 21:40:05 -0500

    REVERT: placenta surg-path

--- routes/pages/surg-path.js
+++ routes/pages/surg-path.js
@@ -45,7 +45,7 @@ router.get('/surg-path/sjogrens', (req, res) => {
 
 router.get('/surg-path/placenta', (req, res) => {
     return render(req, res, './page-views/surg-path/placenta.hbs', [
-		"https://d3js.org/d3.v4.min.js",
+		// "https://d3js.org/d3.v4.min.js",
         "/js/surg-path/placenta-scripts.js",
         "/json/json-placenta.js",
     ]);
